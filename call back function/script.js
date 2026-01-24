0f55f5543f49c8980191a1424b0c5ffc6704abcc
sonamanayakkar <esonaman2@gmail.com>
2026-01-24 09:25:31 +0530

    1st commit

---
+++ call back function/script.js
@@ -0,0 +1,52 @@
+
+
+
+// normal function
+
+// function calculation(a,b){
+//     let sum =a+b;
+//    return sum;
+// }
+
+// console.log(calculation(10,20));
+
+
+// let calculation = function (a,b){
+//     let sum =a+b;
+//    return sum;
+// }
+
+// console.log(calculation(20,20));
+
+
+//arrow function
+
+// let calculation =(a,b)=>{let sum=a+b; return sum;}
+
+// console.log(calculation(5,2))
+
+
+//call-back function
+
+// function calculation(notify, a, b) {
+//     let sum = a + b;
+//    console.log(sum);
+
+//    setTimeout(() => {
+//         notify();
+//    }, 3000);
+
+// }
+
+// function message(){
+//     console.log("addition completed!");
+// }
+
+// calculation(message,5,3);
+
+
+//call-back function example
+
+function shop() {
+
+}
